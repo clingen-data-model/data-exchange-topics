011470dd87deddb142da466cd1a368e934a47a36
Kyle Ferriter <kferrite@broadinstitute.org>
2021-11-16 17:02:48 -0500

    Fix project name and change main to metrics module

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clinvar-streams "0.1.0-SNAPSHOT"
+(defproject data-exchange-topics "0.1.0-SNAPSHOT"
   :description "ClinVar data streams for ClinGen ecosystem applications"
   :url "https://github.com/clingen-data-model/clinvar-streams"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -12,6 +12,7 @@
                  [io.pedestal/pedestal.route "0.5.7"]
                  [io.pedestal/pedestal.jetty "0.5.7"]
                  [org.slf4j/slf4j-simple "1.7.28"]
+                 [org.apache.kafka/kafka-clients "2.8.0"]
                  [mount "0.1.16"]
                  [cli-matic "0.4.3"]
                  [cheshire "5.10.0"]
@@ -25,8 +26,8 @@
                  [org.xerial/sqlite-jdbc "3.32.3.2"]]
   ;:repl-options {:init-ns clinvar-streams.core
   ;               :caught clojure.repl/pst}
-  :main topic-utils.core
-  ;:aot [clinvar-streams.core]
+  :main topic-utils.topic-metrics
+  :aot :all
   :resource-paths ["resources"]
   :target-path "target/%s"
   :auto-clean false
@@ -35,5 +36,4 @@
              ;:testdata {:main clinvar-raw.generate-local-topic
              ;           ;:aot [#"clinvar-raw.*"]
              ;           :repl-options {:init-ns clinvar-raw.generate-local-topic}}
-             }
-  )
+             })
